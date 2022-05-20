efc7822eaebc02b86c43f5cafdce7caf70291774
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-05-20 08:33:03 -0300

    Update atualizar.php

--- fabricantes/atualizar.php
+++ fabricantes/atualizar.php
@@ -4,6 +4,14 @@ require_once '../src/funcoes-fabricantes.php';
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
 $fabricante = lerUmFabricante($conexao, $id);
+
+if(isset($_POST['atualizar'])) {
+    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    atualizarFabricante($conexao, $id, $nome);
+
+    header("location:listar.php");
+}
 ?>
 
 
