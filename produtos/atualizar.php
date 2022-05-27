b20fef7a905c208eef59e3485b501e3edd4a46cd
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-05-27 12:01:15 -0300

    update

--- produtos/atualizar.php
+++ produtos/atualizar.php
@@ -1,7 +1,39 @@
 <?php
 require_once '../src/funcoes-fabricantes.php';
+require_once '../src/funcoes-produtos.php';
 $listaDeFabricantes = lerFabricantes($conexao);
+
+
+/* pegando o valor do id e sanitizando */
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+/* chamando a função e recebendo os dados do produto */
+$produto = lerUmProduto($conexao, $id);
+
+dump($produto);
+
+
+
+/*
+nao sei se ta certo
+ if(isset($_POST['atualizar'])){
+    
+
+    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+    $preco = filter_input(INPUT_POST, 'preco', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    $quantidade = filter_input(INPUT_POST, 'quantidade', FILTER_SANITIZE_NUMBER_INT);
+    $descricao = filter_input(INPUT_POST, 'descricao', FILTER_SANITIZE_SPECIAL_CHARS);
+    $fabricantesId = filter_input(INPUT_POST, 'fabricantes', FILTER_SANITIZE_NUMBER_INT);
+
+    atualizarProduto($conexao, $nome, $preco, $quantidade, $descricao, $fabricantesId);
+
+    header("location:listar.php");
+} */
 ?>
+
+
+
+
 
 
 <!DOCTYPE html>
@@ -20,7 +52,7 @@ $listaDeFabricantes = lerFabricantes($conexao);
         <form action="" method="post">
             <p>
                 <label for="nome">Nome:</label>
-                <input type="text" name="nome" id="nome" required>
+                <input value="<?=$produto['nome']?>" type="text" name="nome" id="nome" required>
             </p>
 
             <p>
