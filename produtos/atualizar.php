29b22cdb5ec26769c717b56c25a650186a7cdc90
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-05-27 10:40:14 -0300

    Create atualizar.php

---
+++ produtos/atualizar.php
@@ -0,0 +1,73 @@
+<?php
+require_once '../src/funcoes-fabricantes.php';
+$listaDeFabricantes = lerFabricantes($conexao);
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos - Atualizar</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Produtos | SELECT/UPDATE</h1>
+        <hr>
+
+        <form action="" method="post">
+            <p>
+                <label for="nome">Nome:</label>
+                <input type="text" name="nome" id="nome" required>
+            </p>
+
+            <p>
+                <label for="preco">Preço:</label>
+                <input type="number" name="preco" id="preco" min="0" max="10000" step="0.01" required>
+            </p>
+
+            <p>
+            <label for="quantidade">Quantidade:</label>
+                <input type="number" name="quantidade" id="quantidade" min="0" max="100" required>
+            </p>
+
+            <p>
+            <label for="fabricante">Fabricante:</label>
+                <select name="fabricantes" id="preco" required>
+                <option value=""></option>
+
+                <!-- programar um foreach para <option value='id'>nome</option> -->
+                <?php
+                foreach($listaDeFabricantes as $fabricantes) {
+                 ?>  
+                 <option value="<?=$fabricantes['id']?>">
+                 <?=$fabricantes['nome']?>  <!-- exibição -->
+                </option>  
+                <?php 
+                } 
+                 ?>
+               
+
+                </select>
+            </p>
+
+            <!-- opções de fabricantes existentes no banco -->
+
+            <p>
+            <label for="descricao">Descrição:</label>
+            <textarea required name="descricao" id="descricao" cols="30" rows="3"></textarea>
+           </p>
+
+            <button type="submit" name="inserir">
+                Atualizar Produtos</button>
+        </form>
+
+        <p><a href="listar.php">
+        Voltar para lista de produtos</a></p>
+
+        <p><a href="../index.php">Home</a></p>
+    </div>
+</body>
+</html>
