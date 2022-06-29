43f7e4017fe1492c61aa50df8563fb9b4788383e
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-06-29 09:36:43 -0300

    atualização

--- fabricantes/atualizar.php
+++ fabricantes/atualizar.php
@@ -1,14 +1,22 @@
 <?php
-require_once '../src/funcoes-fabricantes.php';
+
+use CrudPoo\Fabricante;
+
+require_once '../vendor/autoload.php';
+
+$fabricante = new Fabricante;
 
 // Obtendo o valor do parâmetro da URL
-$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-$fabricante = lerUmFabricante($conexao, $id);
+$fabricante->setId( $_GET['id'] );
+$dadosFabricante = $fabricante->lerUmFabricante();
+
+/* var_dump($dadosFabricante);
+die(); */
 
 if (isset($_POST['atualizar'])) {
-    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS); 
+    $fabricante->setNome($_POST['nome']); 
 
-    atualizarFabricante($conexao, $id, $nome);
+    $fabricante->atualizarFabricante();
 
     //header("location:listar.php");
 
@@ -37,10 +45,10 @@ if (isset($_POST['atualizar'])) {
         <hr>
 
         <form action="" method="post">
-            <input type="hidden" name="<?=$fabricante['id']?>">
+            <input type="hidden" name="<?=$dadosFabricante['id']?>">
             <p>
                 <label for="nome">Nome:</label>
-                <input value="<?=$fabricante['nome']?>" type="text" name="nome" id="nome">
+                <input value="<?=$dadosFabricante['nome']?>" type="text" name="nome" id="nome">
             </p>
             <button type="submit" name="atualizar">
                 Atualizar fabricante</button>
